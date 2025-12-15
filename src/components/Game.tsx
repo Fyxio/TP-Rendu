d330cc0f9c360c509329f5c8f5be47634285d07b
Fyxio <mathieu.sbl@hotmail.com>
2025-12-15 22:09:05 +0100

    Ajout d'un compteur d'erreur

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,12 +1,16 @@
 import HangmanDisplay from "./HangmanDisplay";
 import WordDisplay from "./WordDisplay";
 import Keyboard from "./Keyboard";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { LetterState } from "./Letter";
 
 export default function Game() {
   const [word, setWord] = useState<string>("");
   const [letters, setLetters] = useState<LetterState[]>([]);
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  const MAX_ERRORS = 6;
+  const [errors, setErrors] = useState<number>(0);
+
 
   useEffect(() => {
     const words = ["pomme", "fraise"];
@@ -25,18 +29,31 @@ export default function Game() {
 
   // Quand on clique sur une lettre du clavier
   function handleGuess(letter: string) {
-    setLetters((prev) =>
-      prev.map((l) =>
-        l.display === letter ? { ...l, state: "Display" } : l
-      )
-    );
+  if (guessedLetters.includes(letter)) return;
+
+  setGuessedLetters(prev => [...prev, letter]);
+
+  const isCorrect = letters.some(l => l.display === letter);
+
+  if (!isCorrect) {
+    setErrors(prev => prev + 1);
+    return;
   }
+  setLetters(prev =>
+    prev.map(l =>
+      l.display === letter ? { ...l, state: "Display" } : l
+    )
+  );
+}
 
   return (
     <div className="Game">
-      <WordDisplay letters={letters}/>
-      <Keyboard onGuess={(handleGuess) => console.log("click", handleGuess)}/>
-      {/* <HangmanDisplay /> */}
+        <p>Erreurs : {errors} / {MAX_ERRORS}</p>
+
+        <WordDisplay letters={letters}/>
+
+        <Keyboard onGuess={handleGuess} guessed={guessedLetters} />
+        {/* <HangmanDisplay /> */}
     </div>
   );
 }
